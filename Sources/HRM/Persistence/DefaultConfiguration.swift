5b78a382eaeef328b354d2aef1209acfda06ca91
Wontae Yang <1811312+wontaeyang@users.noreply.github.com>
2026-04-02 23:18:10 -0400

    fix key button click

--- Sources/HRM/Persistence/DefaultConfiguration.swift
+++ Sources/HRM/Persistence/DefaultConfiguration.swift
@@ -5,7 +5,7 @@ enum DefaultConfiguration {
             quickTapTermMs: 150,
             requirePriorIdleMs: 150,
             bilateralFiltering: true,
-            holdTriggerOnRelease: false,
+            holdTriggerOnRelease: true,
             keyBindings: defaultKeyBindings
         )
     }
